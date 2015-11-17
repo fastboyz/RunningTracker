80a60f4442d959333060b669fc3d72f8a589d6c8
Steve Mbiele <stevembiele@gmail.com>
2015-11-17 11:44:20 -0500

    Travail sur la progress Bar fini ou presque.

--- app/src/main/java/ca/qc/bdeb/p45/runningtracker/MainActivity.java
+++ app/src/main/java/ca/qc/bdeb/p45/runningtracker/MainActivity.java
@@ -12,16 +12,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.daimajia.numberprogressbar.NumberProgressBar;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+private NumberProgressBar nbp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        nbp = (NumberProgressBar) findViewById(R.id.MainActivity_progess);
+
+        nbp.setMax(100);
+        nbp.setProgress(60);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
